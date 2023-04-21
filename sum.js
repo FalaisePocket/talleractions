3438b330a42a4c65b0e52fccc8060122f89f36e8
FalaisePocket <victor.nanez@correounivalle.edu.co>
2023-04-21 18:35:58 -0500

    Github-actions configuration to execute a test

--- sum.js
+++ sum.js
@@ -2,4 +2,19 @@ function sum(a, b) {
   return a + b;
 }
 
-module.exports = sum;
+function mult(a,b){
+    return a*b;
+
+}
+
+function hola(){
+    return "Buenos Dias!"
+}
+
+function minorthan(a , b){
+    return a<b;
+}
+function majorthan(a,b){
+    return a>b;
+}
+module.exports = {sum,mult,hola,minorthan,majorthan};
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,18 @@
 const sum = require('./sum');
 
 test('sums two numbers', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum.sum(1, 2)).toBe(3);
 });
+test('mult two numbers', ()=> {
+    expect( sum.mult(2,3)  ).toBe(6);
+});
+test('say Hi',()=>{
+    expect (sum.hola()).toBe("Buenos Dias!");
+})
+
+test('is major than?',()=>{
+    expect (sum.majorthan(5,1)).toBe(true);
+})
+test('is minor than',()=>{
+    expect (sum.minorthan(43,109)).toBe(true);
+})
